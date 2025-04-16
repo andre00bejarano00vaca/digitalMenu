c2b4a54618e923c75b88b9726f8509bc09e4e953
andre00bejarano00vaca <andre.bejarano.vaca@gmail.com>
2025-04-16 14:01:24 -0400

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,10 +18,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased h-full`}
       >
+        <h2 className="text-5xl pl-[5px]">Menú Digital</h2>
+        <p className="pl-[5px]">A continuación, se presenta el menú de servicios de Digital Telecomunicaciones. </p>
         {children}
       </body>
     </html>
